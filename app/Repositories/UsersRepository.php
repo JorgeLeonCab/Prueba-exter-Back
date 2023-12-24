0d83b8472fdef90afe31b8ef4a9d370a1b3cd361
jorgeLeonCab <jorge.leoncab@gmail.com>
2023-12-23 18:37:04 -0600

    Instalacion de librerias de servicios y repositorios

---
+++ app/Repositories/UsersRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class UsersRepository.
+ */
+class UsersRepository extends BaseRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        //return YourModel::class;
+    }
+}
